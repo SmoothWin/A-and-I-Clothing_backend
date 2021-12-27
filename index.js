995b02a75f893b9f19daa7fc656ec466b43dd855
Christian Chitanu <christian.chitanu@hotmail.com>
2021-12-27 01:28:23 -0500

    finished get product endpoints

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet')
 
 const app = express()
 
 //Routes
 const bigOrderRoute = require('./routes/bigorder')
 const authRoute = require('./routes/Auth');
+const stripeRoute = require('./routes/StripeEndpoints')
 
 
+app.use(helmet())
 app.use(express.json({limit:'4mb'}))
 app.use(express.urlencoded({extended: true,limit:'4mb'}))
 app.use(cors({origin:process.env.FRONTEND_URL, credentials:true}));
@@ -17,5 +20,6 @@ app.use(cookieParser())
 //endpoints
 app.use("/bigorders",bigOrderRoute)
 app.use(authRoute)
+app.use(stripeRoute)
 
 module.exports = app
