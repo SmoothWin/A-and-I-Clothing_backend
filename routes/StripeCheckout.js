959afeeb03ba57f30ea419969777c5d6bf3e3a7d
Christian Chitanu <christian.chitanu@hotmail.com>
2022-01-23 17:09:10 -0500

    finished implementation of stripe checkout data being returned when front-end requests session id

--- routes/StripeCheckout.js
+++ routes/StripeCheckout.js
@@ -7,6 +7,7 @@ const easyTokenChecker = require("../middleware/easyTokenChecker")
 
 //custom db imports
 const {getUserEmail} = require('../db/authentication')
+const { array } = require('joi')
 
 
 router.get("/checkout/session", async (req, res)=>{
@@ -15,7 +16,53 @@ router.get("/checkout/session", async (req, res)=>{
             {
                 expand: ['line_items']
             })
-        return res.json(session)
+
+        const line_items = session.line_items
+        const metadata = session.metadata
+        const status = session.payment_status
+
+        const ids = Object.keys(metadata)
+        const products = await stripe.products.list({limit:100,ids:ids})
+        console.log(products)
+
+        const lineItems = line_items.data.map(x=>{
+            return {
+                product_id:x.price.product,
+                price_id:x.price.id,
+                product_name:x.description,
+                unit_price:x.price.unit_amount,
+                subtotal:x.amount_subtotal,
+                total:x.amount_subtotal,
+                total_quantity:x.quantity,
+            }
+        })
+        lineItems.forEach(x=>{
+            products.data.forEach(prod=>{
+                if(prod.id == x.product_id)
+                    x["images"]=prod.images
+            })
+        })
+
+        const secondaryQuantities = Object.entries(metadata).map(x=>{
+            return {
+                [x[0]]:JSON.parse(x[1])
+            }
+        })
+        lineItems.forEach(item => {
+            Object.values(secondaryQuantities).forEach(x=>{
+                Object.entries(x).forEach(value=>{
+                    if(item.product_id == value[0]){
+
+                        console.log()
+                        item["secondary_quantities"] = value[1]
+                    }
+                })
+               
+            })
+        });
+        
+
+        return res.json({line_items:lineItems, status:status})
     }catch(e){
         console.log(e)
         return res.status(400).json({message:"sessionId doesn't exist or has expired"})
@@ -39,7 +86,7 @@ router.post("/checkout", easyTokenChecker,async (req, res)=>{
         items.forEach((item)=>{
             metadata[item.product_id] = JSON.stringify(item.category_quantities)
             lineList.push({
-                price: `price_${item.id}`,
+                price: `${item.id}`,
                 quantity:item.tot_quantity
             })
         })
